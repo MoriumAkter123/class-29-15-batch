f4200d6621b8e197eebd805dd63b70ed06887f6b
MoriumAkter123 <morium2312091068@diu.edu.bd>
2024-10-19 00:01:18 +0600

    class-29

---
+++ script.js
@@ -0,0 +1,43 @@
+
+let indexArray = [43,98,34,56,33,65];
+ console.log("Number is:",indexArray[4]);//33
+ console.log("Number is:",indexArray[0]);
+ console.log("Number is:",indexArray[7]);
+
+ let student = {name : "Rahim", age : 32, blood : "o+"};
+
+ console.log("His Age is : " , student.age);
+ console.log("His name is : " , student.name);
+
+ let twoDimentionalArray = [
+
+               [43,98,32,3],
+               [11,81,39,47],
+               [12,5,100,47],
+               [91,1,2,0]
+               
+
+ ];
+
+
+ console.log("value is :", twoDimentionalArray[0][0]);
+ console.log("value is :", twoDimentionalArray[0][1]);
+ console.log("value is :", twoDimentionalArray[0][2]);
+ console.log("value is :", twoDimentionalArray[0][3]);
+ 
+ console.log("value is :", twoDimentionalArray[1][0]);
+ console.log("value is :", twoDimentionalArray[1][1]);
+ console.log("value is :", twoDimentionalArray[1][2]);
+ console.log("value is :", twoDimentionalArray[1][3]);
+
+ 
+ console.log("value is :", twoDimentionalArray[2][0]);
+ console.log("value is :", twoDimentionalArray[2][1]);
+ console.log("value is :", twoDimentionalArray[2][2]);
+ console.log("value is :", twoDimentionalArray[2][3]);
+
+ 
+ console.log("value is :", twoDimentionalArray[3][0]);
+ console.log("value is :", twoDimentionalArray[3][1]);
+ console.log("value is :", twoDimentionalArray[3][2]);
+ console.log("value is :", twoDimentionalArray[3][3]);
